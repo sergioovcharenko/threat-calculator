102823a2bcacf04ddbcbd6b6baf5e43d6c147b3a
sergioovcharenko <sergioovcharenko@gmail.com>
2026-09-20 22:44:07 +0300

    Add server.js for Railway hosting

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log('Threat Calculator running on port ' + port);
+});
